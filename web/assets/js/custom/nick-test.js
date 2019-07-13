a050af056c7d5f7a77773968cdaec81579baecc8
hiskander aguillon <hiskander@gmail.com>
2019-07-13 09:27:28 -0300

    Register validations Works!

--- web/assets/js/custom/nick-test.js
+++ web/assets/js/custom/nick-test.js
@@ -7,14 +7,15 @@ $(document).ready(function () {
             url: URL + '/nick-test',
             data: {nick: nick},
             type: 'POST',
-            success: function(response) {
-                if(response == "used"){
-                    $(".nick-input").css("border", "1px solid red");
-                }else{
-                    $(".nick-input").css("border", "1px solid green");
+            success: function (response) {
+                if (response == "used") {
+                    $(".nick-input").css("border-left", "10px solid red");
+                } else {
+                    $(".nick-input").css("border-left", "5px solid green");
                 }
             }
         });
+
     });
 
 });
