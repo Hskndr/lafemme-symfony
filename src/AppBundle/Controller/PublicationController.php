1c71ef19419d08ddd8cc5dbbcde73137ffe2553b
hiskander aguillon <hiskander@gmail.com>
2019-07-14 09:38:29 -0300

    Timeline and Publications 40%

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -2,15 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use BackendBundle\Entity\Publication;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\PublicationType;
 
 class PublicationController extends Controller
 {
+
 //    ROUTING TO PUBLICATION
-    public function indexAction(Request $request){
-        return $this->render('AppBundle:Publication:home.html.twig');
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $publication = new Publication();
+        $form = $this->createForm(PublicationType::class, $publication);
+
+
+        return $this->render('AppBundle:Publication:home.html.twig',array(
+            'form' => $form->createView()
+        ));
 
     }
 
